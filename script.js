bbd5d14dc98235c5dc220ed830f2c43de713d5df
Ayan Abdimutalip <aianabdimutalip6@gmail.com>
2024-11-23 10:04:48 +0500

    Ayan2

--- script.js
+++ script.js
@@ -132,82 +132,82 @@
 
 
 //                TAPSYRMA 9
-// const bergerKing = () => {
-//     let kaspi = +prompt("Қандай счетыңыздан төлем жасайсыз \n1) KZT\n2) USD \n3) CNY \nСчетыңызды таңдау үшін оның реттік номерін жазыңыз");
-//     let kaspiKZT = 10000;
-//     let KaspiUSD = 500;
-//     let kaspiCNY = 7600;
-//     let kaspiNEW = 0;
+const bergerKing = () => {
+    let kaspi = +prompt("Қандай счетыңыздан төлем жасайсыз \n1) KZT\n2) USD \n3) CNY \nСчетыңызды таңдау үшін оның реттік номерін жазыңыз");
+    let kaspiKZT = 10000;
+    let KaspiUSD = 500;
+    let kaspiCNY = 7600;
+    let kaspiNEW = 0;
 
 
-//     if (kaspi == 2) {
-//         kaspiNEW = KaspiUSD * 500;
-//     } else if (kaspi == 1) {
-//         kaspiNEW = kaspiKZT * 1;
-//     } else if (kaspi == 3) {
-//         kaspiNEW = kaspiCNY * 60;
-//     } else {
-//         alert("Қате таңдадыңыз!");
-//         return;
-//     }
+    if (kaspi == 2) {
+        kaspiNEW = KaspiUSD * 500;
+    } else if (kaspi == 1) {
+        kaspiNEW = kaspiKZT * 1;
+    } else if (kaspi == 3) {
+        kaspiNEW = kaspiCNY * 60;
+    } else {
+        alert("Қате таңдадыңыз!");
+        return;
+    }
 
-//     while (true) {
-//         let menu = prompt("Тапсырыс беріңіз: \n1)Бургер 1500 KZT\n2)ЧизБургер 1200 KZT\n3)Донер 1300 KZT\n4)Coca cola 500 KZT\n5)Fnata 500 KZT\n6)Sprite 500 KZT\n7)Garila 600 KZT\n8)Pepse 500 KZT\n9)Чай 300 KZT\n10)Лимонад 450 KZT\nТағамды таңдау үшін тағамның реттік номерін жазыңыз\nегер тапсырыс бермейтін болсаңыз 'stop' - деп жазыңыз");
+    while (true) {
+        let menu = prompt("Тапсырыс беріңіз: \n1)Бургер 1500 KZT\n2)ЧизБургер 1200 KZT\n3)Донер 1300 KZT\n4)Coca cola 500 KZT\n5)Fnata 500 KZT\n6)Sprite 500 KZT\n7)Garila 600 KZT\n8)Pepse 500 KZT\n9)Чай 300 KZT\n10)Лимонад 450 KZT\nТағамды таңдау үшін тағамның реттік номерін жазыңыз\nегер тапсырыс бермейтін болсаңыз 'stop' - деп жазыңыз");
 
-//         if (menu == "stop") {
-//             break;
-//         }
+        if (menu == "stop") {
+            break;
+        }
 
-//         let price = 0;
-//         let item = "";
+        let price = 0;
+        let item = "";
 
-//         if (menu == "1") {
-//             price = 1500;
-//             item = "Бургер";
-//         } else if (menu == "2") {
-//             price = 1200;
-//             item = "ЧизБургер";
-//         } else if (menu == "3") {
-//             price = 1300;
-//             item = "Донер";
-//         } else if (menu == "4") {
-//             price = 500;
-//             item = "Coca cola";
-//         } else if (menu == "5"){
-//             price = 500;
-//             item = "FAnta";
-//         } else if (menu == "6"){
-//             price = 500;
-//             item = "Sprite";
-//         } else if (menu == "7"){
-//             price = 500;
-//             item = "Garila";
-//         }else if (menu == "8"){
-//             price = 500;
-//             item = "Pepse";
-//         }else if (menu == "9"){
-//             price = 500;
-//             item = "Чай";
-//         }else if (menu == "10"){
-//             price = 500;
-//             item = "лимонад";
-//         }
+        if (menu == "1") {
+            price = 1500;
+            item = "Бургер";
+        } else if (menu == "2") {
+            price = 1200;
+            item = "ЧизБургер";
+        } else if (menu == "3") {
+            price = 1300;
+            item = "Донер";
+        } else if (menu == "4") {
+            price = 500;
+            item = "Coca cola";
+        } else if (menu == "5"){
+            price = 500;
+            item = "FAnta";
+        } else if (menu == "6"){
+            price = 500;
+            item = "Sprite";
+        } else if (menu == "7"){
+            price = 500;
+            item = "Garila";
+        }else if (menu == "8"){
+            price = 500;
+            item = "Pepse";
+        }else if (menu == "9"){
+            price = 500;
+            item = "Чай";
+        }else if (menu == "10"){
+            price = 500;
+            item = "лимонад";
+        }
 
-//         if (kaspiNEW < price) {
-//             alert("Счетыңызда ақша жеткіліксіз");
-//             break;
-//         }
+        if (kaspiNEW < price) {
+            alert("Счетыңызда ақша жеткіліксіз");
+            break;
+        }
 
-//         kaspiNEW -= price;
-//         resault += price;
-//         ChekList += menu + ") " + item + "\n";
-//         alert("Төлем қабылданды. Сізде қалған ақша: " + kaspiNEW + " KZT");
-//     }
+        kaspiNEW -= price;
+        resault += price;
+        ChekList += menu + ") " + item + "\n";
+        alert("Төлем қабылданды. Сізде қалған ақша: " + kaspiNEW + " KZT");
+    }
 
-//     alert("Сіз тапсырыс берген тағамдар тізімі: \n" + ChekList + "\nСіздің счетыңыздан " + resault + " KZT ақша шешіп алынды");
-// };
+    alert("Сіз тапсырыс берген тағамдар тізімі: \n" + ChekList + "\nСіздің счетыңыздан " + resault + " KZT ақша шешіп алынды");
+};
 
-// bergerKing();
+bergerKing();
 
 
 //                TAPSYRMA 10
